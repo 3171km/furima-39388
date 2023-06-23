0616616a86f1e9bcf17207240d4ae47bd821e4f2
3171km <kyusemasahiro@gmail.com>
2023-06-24 08:05:24 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,22 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new]
+before_action :authenticate_user!, only: [:new]
 
   def index 
+    @items = Item.all.order("created_at DESC")
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -21,5 +29,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+  end
+
+private
+
+def item_params
+  params.require(:item).permit(:image, :title, :overview, :category_id, :status_id, :burden_id, :region_id, :shipment_id,:price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,23 @@
 class Item < ApplicationRecord
+  
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :region
+  belongs_to :shipment
+
+  validates :title,    presence: true
+  validates :overview, presence: true
+  validates :image,    presence: true
+  validates :category_id,  numericality: { other_than: 1 , message: "can't be blank"}
+  validates :status_id,    numericality: { other_than: 1 , message: "can't be blank"}
+  validates :burden_id,    numericality: { other_than: 1 , message: "can't be blank"}
+  validates :region_id ,   numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipment_id,  numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price,        numericality: { greater_than: 299, less_than: 10_000_000, only_integer: true }
 end
