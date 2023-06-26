20c48332063697c9a1170af66390543ec980c113
3171km <kyusemasahiro@gmail.com>
2023-06-26 17:17:17 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,40 +1,48 @@
 class ItemsController < ApplicationController
-
-before_action :authenticate_user!, only: [:new]
-
-  def index 
-    @items = Item.all.order(created_at: :desc)
-  end
-
-  def new
-    @item = Item.new
-  end
-
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
+  
+  before_action :authenticate_user!, only: [:new]
+  
+    def index 
+      @items = Item.all.order(created_at: :desc)
     end
-  end
-
-  def show
-    @item = Item.find(params[:id])
-  end
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
-private
-
-def item_params
-  params.require(:item).permit(:image, :title, :overview, :category_id, :status_id, :burden_id, :region_id, :shipment_id,:price).merge(user_id: current_user.id)
-  end
+  
+    def new
+      @item = Item.new
+    end
+  
+    def create
+      @item = Item.new(item_params)
+      if @item.save
+        redirect_to root_path
+      else
+        render :new
+      end
+    end
+  
+    def show
+      @item = Item.find(params[:id])
+    end
+  
+    def edit
+      @item = Item.find(params[:id])
+    end
+  
+    def update
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
+        redirect_to item_path(@item.id)
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+    end
+  
+  
+  private
+  
+    def item_params
+    params.require(:item).permit(:image, :title, :overview, :category_id, :status_id, :burden_id, :region_id, :shipment_id,:price).merge(user_id: current_user.id)
+    end
+    end
