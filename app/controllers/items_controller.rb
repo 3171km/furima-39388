42ce9fd3468c1bda44600416ab409bb9a8bbc9ce
3171km <kyusemasahiro@gmail.com>
2023-06-27 14:37:58 +0900

    商品情報編集機能4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,58 +1,55 @@
 class ItemsController < ApplicationController
   
-  before_action :authenticate_user!, only: [:new]
-  
-    def index 
-      @items = Item.all.order(created_at: :desc)
-    end
-  
-    def new
-      @item = Item.new
-    end
-  
-    def create
-      @item = Item.new(item_params)
-      if @item.save
-        redirect_to root_path
-      else
-        render :new
-      end
-    end
-  
-    def show
-      @item = Item.find(params[:id])
-    end
-  
-    def edit
-      @item = Item.find(params[:id])
-      
-      if user_signed_in?
-        if current_user.id == @item.user_id
-          render :edit
-        else
-          redirect_to root_path, alert: "他のユーザーの商品は編集できません。"
-        end
-      else
-        redirect_to new_user_session_path, alert: "商品を編集するにはログインしてください。"
-      end
-    end
-  
-    def update
-      @item = Item.find(params[:id])
-      if @item.update(item_params)
-        redirect_to item_path(@item.id)
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-    end
-  
-  
-  private
-  
-    def item_params
-    params.require(:item).permit(:image, :title, :overview, :category_id, :status_id, :burden_id, :region_id, :shipment_id,:price).merge(user_id: current_user.id)
-    end
-    end
+before_action :authenticate_user!, only: [:new, :edit]
+before_action :find_item, only: [:show, :edit, :update]
+
+def index 
+  @items = Item.all.order(created_at: :desc)
+end
+
+def new
+  @item = Item.new
+end
+
+def create
+  @item = Item.new(item_params)
+  if @item.save
+    redirect_to root_path
+  else
+    render :new
+  end
+end
+
+def show
+end
+
+def edit
+  if current_user.id == @item.user_id
+    render :edit
+  else
+    redirect_to root_path, alert: "他のユーザーの商品は編集できません。"
+  end
+end
+
+def update
+  if @item.update(item_params)
+    redirect_to item_path(@item.id)
+  else
+    render :edit
+  end
+end
+
+def destroy
+end
+
+
+private
+
+def item_params
+  params.require(:item).permit(:image, :title, :overview, :category_id, :status_id, :burden_id, :region_id, :shipment_id,:price).merge(user_id: current_user.id)
+end
+
+def find_item
+  @item = Item.find(params[:id])
+end
+end
